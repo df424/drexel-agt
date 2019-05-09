9ca45ee45c8826bb9e2eda33f9d708adc42af1c9
df424 <d.l.flanagan01@gmail.com>
2019-05-09 10:23:08 -0400

    J/K Fixed the bug in the random policy initialization now.0

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ from scipy.special import softmax
 
 def _getInitialPolicy(n_actions, args):
     if(args.random_start):
-        print(np.random.rand(n_actions)*abs(args.random_start_max-args.random_start_min)+args.random_start_min)
-        return np.random.rand(n_actions)*abs(args.random_start_max-args.random_start_min)-args.random_start_min
+        return np.random.rand(n_actions)*abs(args.random_start_max-args.random_start_min)+args.random_start_min
 
     return np.zeros(n_actions) 
 
